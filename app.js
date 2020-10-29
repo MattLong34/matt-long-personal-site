e11d2fa04a9712f6a08bb07d7b6dbac74a22dc40
MattLong34 <mdlong34@gmail.com>
2020-10-29 12:35:57 -0600

    add header responsiveness

---
+++ app.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('nav-toggler')
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+})
